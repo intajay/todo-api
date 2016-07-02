14faad9c5b39258ac4ccbfb330ab425f4b1fef12
intajay <kataravat@gmail.com>
2016-07-03 04:33:56 +0530

    Add DELETE for todos

--- server.js
+++ server.js
@@ -22,12 +22,12 @@ app.get('/todos', function(req, res) {
 app.get('/todos/:id', function(req, res) {
 	// Convert req.params.id string into number
 	var todoId = parseInt(req.params.id, 10);
-	var matchedId = _.findWhere(todos, {id: todoId});
+	var matchedTodo = _.findWhere(todos, {id: todoId});
 
-	if(matchedId) {
-		res.json(matchedId);
+	if(matchedTodo) {
+		res.json(matchedTodo);
 	} else {
-		res.status(404).send('404');
+		res.status(404).json({"error": "no todo found with that id"});
 	}
 });
 
@@ -35,7 +35,7 @@ app.post('/todos', function(req, res) {
 	body = _.pick(req.body, 'description', 'completed');
 
 	if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-		return res.status(400).send(400);
+		return res.status(400).json({"error": "can't add todo"});
 	}
 
 	body.description = body.description.trim();
@@ -43,6 +43,18 @@ app.post('/todos', function(req, res) {
 	todos.push(body);
 	console.log('Description : ' + body.description);
 	res.json(body);
+});
+
+app.delete('/todos/:id', function(req, res) {
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+
+	if(!matchedTodo) {
+		res.status(404).json({"error": "no todo found with that id"});
+	} else {
+		todos = _.without(todos, matchedTodo);
+		res.json(matchedTodo);
+	}
 });
 
 app.listen(server_port, server_ip_address, function () {
