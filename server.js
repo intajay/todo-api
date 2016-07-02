391bde491e3f0a07f8f34191266ec8455db0e4ac
intajay <kataravat@gmail.com>
2016-07-03 02:00:51 +0530

    Add POST for todos

--- server.js
+++ server.js
@@ -1,21 +1,14 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
-var todos = [{
-	id: 1,
-	description: 'first todo description',
-	completed: false
-}, {
-	id: 2,
-	description: 'second todo description',
-	completed: false
-}, {
-	id: 3,
-	description: 'third todo description',
-	completed: true
-}];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
     res.send('Todo app!');
@@ -40,6 +33,14 @@ app.get('/todos/:id', function(req, res) {
 	} else {
 		res.status(404).send('Not Found');
 	}
+});
+
+app.post('/todos', function(req, res) {
+	var body = req.body;
+	body.id = todoNextId++;
+	todos.push(body);
+	console.log('Description : ' + body.description);
+	res.json(body);
 });
 
 app.listen(server_port, server_ip_address, function () {
