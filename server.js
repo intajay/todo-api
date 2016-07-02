58be15f0fd56d51f6b0091ed1bfe8027955de10f
intajay <kataravat@gmail.com>
2016-07-02 17:51:50 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
+app.get('/', function(req, res) {
+    res.send('Todo app');
+});
+
+app.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", server_port " + server_port );
+});
