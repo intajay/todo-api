51d39248cb6b77ee2f08c64d459878cf64697a35
intajay <kataravat@gmail.com>
2016-07-02 19:16:30 +0530

    Update language

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 app.get('/', function(req, res) {
-    res.send('Todo app');
+    res.send('Todo app!');
 });
 
 app.listen(server_port, server_ip_address, function () {
