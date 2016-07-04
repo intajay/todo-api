202dd9441c0954fcc6e504074f13e1b64ea2de0b
intajay <kataravat@gmail.com>
2016-07-05 05:13:56 +0530

    Add  PostgreSQL

--- db.js
+++ db.js
@@ -1,8 +1,31 @@
 var Sequelize = require('sequelize');
-var sequelize = new Sequelize(undefined, undefined, undefined, {
-	'dialect': 'sqlite',
-	'storage': __dirname + '/data/db.sqlite'
-});
+//var env = process.env.OPENSHIFT_ENV_VAR || 'development';
+
+//console.log(process.env.OPENSHIFT_ENV_VAR);
+
+// connection_string = '://127.0.0.1:3000/' + todo;
+// if (process.env.OPENSHIFT_POSTGRESQL_DB_URL) {
+// 	connection_string = process.env.OPENSHIFT_POSTGRESQL_DB_URL + todo;
+// }
+
+var sequelize = new Sequelize('todo', 'adminwhlmkm7', 'dthVplJ6Hqfr', {
+		host: 'localhost',
+		port: 3000,
+		dialect: 'postgresql'
+	});
+
+// if (env == 'production') {
+// 	sequelize = new Sequelize('todo', 'adminwhlmkm7', 'dthVplJ6Hqfr', {
+// 		host: 'localhost',
+// 		port: 3000,
+// 		dialect: 'postgresql'
+// 	});
+// } else {
+// 	sequelize = new Sequelize(undefined, undefined, undefined, {
+// 		dialect: 'sqlite',
+// 		storage: __dirname + '/data/db.sqlite'
+// 	});
+// }
 
 var db = {};
 
