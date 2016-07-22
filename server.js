27ab0d6bb6bfffbbb9e6889067294952330e05b9
intajay <kataravat@gmail.com>
2016-07-23 04:10:03 +0530

    enable CORS

--- server.js
+++ server.js
@@ -13,6 +13,13 @@ var todoNextId = 1;
 
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	next();
+});
+
 app.get('/', function(req, res) {
 	res.send('Todo app!');
 });
